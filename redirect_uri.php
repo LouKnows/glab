426621357292ae32fc0ffab62d40b45dedfab925
LourenceI3Laptop <lourence.johnc@gmail.com>
2019-11-27 03:39:33 +0800

    updated redirect_uri to handle unsubscribe users

--- redirect_uri.php
+++ redirect_uri.php
@@ -2,12 +2,25 @@
 
 require 'dbconnect.php';
 
-$subscriber_token = $_GET['access_token'];
-$subscriber_number = $_GET['subscriber_number'];
+# if user subscribes, store their access_token
+# on db so that we can reply to them
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	if(isset($_GET['access_token']) && $_GET['subscriber_number']){
+		$subscriber_token = $_GET['access_token'];
+		$subscriber_number = $_GET['subscriber_number'];
 
-$sql = "insert into subscribers (access_token, mobile_number) values (#{$subscriber_token},#{$subscriber_number})";
+		$sql = "insert into subscribers (access_token, mobile_number) values (#{$subscriber_token},#{$subscriber_number})";
 
-$conn->query($sql);
+		$conn->query($sql);
+	}
+}
+
+# if request is post, meaning, the user has unsubscribed the app
+# we need to remove the data from the database
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+}
+
 $conn->close();
 
 ?>
