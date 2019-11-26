050125c5dfb7092a3937f13bdb4e90b7e5324401
LourenceI3Laptop <lourence.johnc@gmail.com>
2019-11-26 11:50:47 +0800

    updated connection

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,15 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'globelab';
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 
 // create database connection
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($hostname, $username, $password, $database);
 
 if (mysqli_connect_error()) {
     die("Database connection failed: " . mysqli_connect_error());
