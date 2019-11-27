984b2cf7390c7906e9540285f9064b3144ad616d
LourenceI3Laptop <lourence.johnc@gmail.com>
2019-11-27 17:25:04 +0800

    added unsubscribe functionality

--- redirect_uri.php
+++ redirect_uri.php
@@ -18,7 +18,17 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
 # if request is post, meaning, the user has unsubscribed the app
 # we need to remove the data from the database
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$json_str = file_get_contents('php://input');
+	$json_obj = json_decode($json_str, true);
 
+	$number = $json_obj['unsubscribed']['subscriber_number'];
+	$number = '+63' . $number;
+
+	$sql_remove_subscriber = "delete from subscribers where mobile_number = '$number';";
+	$sql_remove_messages = "delete from messages where sender = '$number';";
+	$sql = $sql_remove_subscriber . $sql_remove_messages;
+
+	$conn->query($sql);
 }
 
 $conn->close();
