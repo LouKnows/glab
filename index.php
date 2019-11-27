4ad9fa73f837d0e4d1ea74ddd7113dca3a227ddb
LourenceI3Laptop <lourence.johnc@gmail.com>
2019-11-27 15:44:18 +0800

    added style

--- index.php
+++ index.php
@@ -8,20 +8,57 @@
 <html>
 <head>
 	<title>GlobeLab Sample</title>
+	<style type="text/css">
+		.body{
+			background-color: #f2f2f2;
+			font-family: sans-serif;
+		}
+		.container{
+			width: 80%;
+			height: 100vh;
+			margin: auto;
+			position: relative;
+		}
+
+		li{
+			padding: 10px;
+			border-bottom: 1px solid #cccccc;
+		}
+
+		span{
+			background-color: #d9edf7;
+			border-color: #bce8f1;
+			color: #31708f;
+			margin-right: 30px;
+			border-radius: 5px;
+			padding: 5px;
+		}
+		a{
+			color: inherit;
+			text-decoration: none;
+		}
+		span:hover{
+			background-color: #d1f0ff;
+		}
+	</style>
 </head>
 <body>
-	<h4>Messages:</h4>
-	<ul>
-		<?php
-			foreach ($messages as $msg) {
-				$to = $msg['sender'];
-				$token = $msg['access_token'];
-				echo '<li>';
-				echo $msg['text_msg'];
-				echo "<span><a href='reply_form.php?to=$to&token=$token'>Reply</a></span>";
-				echo '</li>';
-			}
-		?>
-	</ul>
+	<div class="container">
+		<div class="messages">
+			<h4>Messages:</h4>
+			<ul>
+				<?php
+					foreach ($messages as $msg) {
+						$to = $msg['sender'];
+						$token = $msg['access_token'];
+						echo '<li>';
+						echo $msg['text_msg'];
+						echo "<span><a href='reply_form.php?to=$to&token=$token'>Reply</a></span>";
+						echo '</li>';
+					}
+				?>
+			</ul>
+		</div>
+	</div>
 </body>
 </html>
