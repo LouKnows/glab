5c028c367bcc5cd72ea44b016c9b92732a0e0e4e
LourenceI3Laptop <lourence.johnc@gmail.com>
2019-11-27 16:07:53 +0800

    style

--- reply_form.php
+++ reply_form.php
@@ -9,11 +9,15 @@
 	<title>Reply Form</title>
 	<style type="text/css">
 		.container{
-			background-color: #cce6ff;
+			height: 100vh;
+			width: 100%;
+			position: relative;
+		}
+		.form{
+			border: 1px solid #cce6ff;
 			padding: 30px;
 			width: 200px;
 			height: 150px;
-			margin: auto;
 			position: absolute;
 			top: 50%;
 			left: 50%;
@@ -21,6 +25,7 @@
 			color: #31708f;
 			font-family: sans-serif;
 			margin-top: 50%;
+			border-radius: 5px;
 		}
 
 		.btn{
@@ -34,18 +39,20 @@
 </head>
 <body>
 	<div class="container">
-		<form action="send_reply.php" method="POST">
-			<p>To: <?php echo $to; ?></p>
-			<input type="hidden" name="address" value="<?php echo $to; ?>" >
-			<input type="hidden" name="access_token" value="<?php echo $token; ?>" >
-			<p>Message:</p>
-			<p>
-				<textarea name='outbound-msg'></textarea>
-			</p>
-			<p>
-				<input class="btn" type="submit" value="Send Reply">
-			</p>
-		</form>
+		<div class="form">
+			<form action="send_reply.php" method="POST">
+				<p>To: <?php echo $to; ?></p>
+				<input type="hidden" name="address" value="<?php echo $to; ?>" >
+				<input type="hidden" name="access_token" value="<?php echo $token; ?>" >
+				<p>Message:</p>
+				<p>
+					<textarea name='outbound-msg'></textarea>
+				</p>
+				<p>
+					<input class="btn" type="submit" value="Send Reply">
+				</p>
+			</form>
+		</div>
 	</div>
 </body>
 </html>
